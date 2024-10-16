42457d0df46d5759e64a60e2daf5df39a4ba28b1
aholodov <holodov.pixel@gmail.com>
2024-10-16 12:33:30 +0300

    feat(improvement): remove unused header and footer

--- src/components/main-menu-ui-fragment.tsx
+++ src/components/main-menu-ui-fragment.tsx
@@ -1,13 +1,7 @@
-import { Button } from '@/components/ui/button.tsx'
-
 export default function MainMenuUiFragment() {
   return (
-    <>
-      <div className="flex min-h-60 justify-center items-center bg-purple-500 rounded">
-        Main menu UI block
-      </div>
-
-      <Button>logout</Button>
-    </>
+    <div className="flex min-h-60 justify-center items-center bg-purple-500 rounded">
+      Main menu UI block
+    </div>
   )
 }
