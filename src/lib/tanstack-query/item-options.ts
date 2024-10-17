fd0c3e1207c52b290c950dae92645bdac290096d
aholodov <holodov.pixel@gmail.com>
2024-10-17 16:52:30 +0300

    feat(improvement): use generatePath in item query

--- src/lib/tanstack-query/item-options.ts
+++ src/lib/tanstack-query/item-options.ts
@@ -1,6 +1,7 @@
 import { queryOptions } from '@tanstack/react-query'
+import { generatePath } from 'react-router-dom'
 import { z } from 'zod'
-import axiosInstance from '@/lib/axios-instance.ts'
+import axiosInstance from '@/lib/axios-instance'
 
 /* region [Recursive types](https://github.com/colinhacks/zod?tab=readme-ov-file#recursive-types) */
 const baseItemSchema = z.object({
@@ -36,7 +37,7 @@ export default function createItemOptions({ itemId }: ItemOptionProps) {
   return queryOptions({
     queryKey: itemsWithIdKey({ itemId }),
     queryFn: async ({ queryKey: [path, { itemId }], signal }) => {
-      const url = path.replace(':itemId', itemId)
+      const url = generatePath(path, { itemId })
       const response = await axiosInstance.get(url, {
         signal,
       })
